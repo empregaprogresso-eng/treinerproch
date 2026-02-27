3d064c055d99b2a63054b270c9edf0d213d8bc60
empregaprogresso-eng <empregaprogresso@gmail.com>
2026-02-27 17:08:14 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,21 @@
 
-self.addEventListener("install", e=>{
-  e.waitUntil(caches.open("fitcache").then(cache=>cache.addAll(["/"])))
+const CACHE_NAME = "fitsaas-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
