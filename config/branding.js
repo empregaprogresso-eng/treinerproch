feb84f225da8fb5dc88efd088d63b1e6c91ca551
empregaprogresso-eng <empregaprogresso@gmail.com>
2026-02-27 13:13:00 -0300

    Add files via upload

--- config/branding.js
+++ config/branding.js
@@ -1,1 +1,9 @@
 
+// 🎨 PERSONALIZAÇÃO WHITE LABEL
+
+const BRAND = {
+  name: "Seu Personal Trainer",
+  primaryColor: "#0f172a",
+  secondaryColor: "#22c55e",
+  logo: "../assets/logo.png"
+};
---
+++ config/firebase.js
@@ -0,0 +1,14 @@
+
+// 🔥 COLE AQUI SUAS CONFIGURAÇÕES DO FIREBASE
+const firebaseConfig = {
+  apiKey: "COLE_AQUI",
+  authDomain: "COLE_AQUI",
+  projectId: "COLE_AQUI",
+  storageBucket: "COLE_AQUI",
+  messagingSenderId: "COLE_AQUI",
+  appId: "COLE_AQUI"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
