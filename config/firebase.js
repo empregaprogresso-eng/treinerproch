62cde71b758185162505819c49e44811e9911b77
empregaprogresso-eng <empregaprogresso@gmail.com>
2026-02-27 14:37:44 -0300

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,14 +1,12 @@
 
 const firebaseConfig = {
-  apiKey: "COLE_AQUI",
-  authDomain: "COLE_AQUI",
-  projectId: "COLE_AQUI",
-  storageBucket: "COLE_AQUI",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI",
-  measurementId: "COLE_AQUI"
+  apiKey: "AIzaSyDU5tNXOQF9i0F_uuEyBvqWsNtmhgl9jrY",
+  authDomain: "treinerproch.firebaseapp.com",
+  projectId: "treinerproch",
+  storageBucket: "treinerproch.firebasestorage.app",
+  messagingSenderId: "497545364595",
+  appId: "1:497545364595:web:bb634bf57a77bb56841606"
 };
-
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
