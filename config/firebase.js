2134e13fbf5e711abd95062e4afbf3b230c60e91
empregaprogresso-eng <empregaprogresso@gmail.com>
2026-02-27 16:34:59 -0300

    Add files via upload

--- config/firebase.js
+++ config/firebase.js
@@ -7,8 +7,8 @@ const firebaseConfig = {
   messagingSenderId: "497545364595",
   appId: "1:497545364595:web:bb634bf57a77bb56841606"
 };
+
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
-const analytics = firebase.analytics();
