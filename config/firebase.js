878ea7da00112e2edffeb3d72393f05a6787e8cc
empregaprogresso-eng <empregaprogresso@gmail.com>
2026-02-27 14:10:50 -0300

    Add files via upload

--- config/firebase.js
+++ config/firebase.js
@@ -1,25 +1,16 @@
 
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDU5tNXOQF9i0F_uuEyBvqWsNtmhgl9jrY",
-    authDomain: "treinerproch.firebaseapp.com",
-    projectId: "treinerproch",
-    storageBucket: "treinerproch.firebasestorage.app",
-    messagingSenderId: "497545364595",
-    appId: "1:497545364595:web:bb634bf57a77bb56841606"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+const firebaseConfig = {
+  apiKey: "COLE_AQUI",
+  authDomain: "COLE_AQUI",
+  projectId: "COLE_AQUI",
+  storageBucket: "COLE_AQUI",
+  messagingSenderId: "COLE_AQUI",
+  appId: "COLE_AQUI",
+  measurementId: "COLE_AQUI"
+};
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
+const analytics = firebase.analytics();
