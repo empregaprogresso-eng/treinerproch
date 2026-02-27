d1da18cce724f11c3b8850ffaa387166025c2afd
empregaprogresso-eng <empregaprogresso@gmail.com>
2026-02-27 13:58:42 -0300

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,12 +1,23 @@
 
-const firebaseConfig = {
-  apiKey: "COLE_AQUI",
-  authDomain: "COLE_AQUI",
-  projectId: "COLE_AQUI",
-  storageBucket: "COLE_AQUI",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDU5tNXOQF9i0F_uuEyBvqWsNtmhgl9jrY",
+    authDomain: "treinerproch.firebaseapp.com",
+    projectId: "treinerproch",
+    storageBucket: "treinerproch.firebasestorage.app",
+    messagingSenderId: "497545364595",
+    appId: "1:497545364595:web:bb634bf57a77bb56841606"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
